4ab75302b1118c5dbb2c6706d46db272c0fef884
Fillipy Saraiva <lip-crs@hotmail.com>
2022-12-11 21:59:59 -0300

    feature-contato-localstorage
    Implementado sistema de recuperação de mensagens com Local Storage

--- scriptlistmsg.js
+++ scriptlistmsg.js
@@ -1,5 +1,18 @@
-getLocalStorage();
 const botaoVoltar = document.querySelector("#voltarContato");
 botaoVoltar.addEventListener("click", function () {
-  window.location.assign = localStorage.getItem("link");
+  localStorage.clear();
+  document.location.href = "contato.html";
 });
+
+const botaoCarregar = document.querySelector("#carregarMensagens");
+botaoCarregar.addEventListener("click", function () {
+  var mensagensRecebidas = JSON.parse(localStorage.getItem("mensagens"));
+  console.log(mensagensRecebidas);
+  for(let i = 0; i < mensagensRecebidas.length; i++)
+  {
+  document.querySelector("#recebendoMensagens").innerHTML += "Nome : " + mensagensRecebidas[i].nome + " Mensagem : " + mensagensRecebidas[i].mensagem;
+  document.querySelector("#recebendoMensagens").innerHTML += "<br>";
+  }
+  
+
+})
